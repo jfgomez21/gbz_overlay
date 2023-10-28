46f7061d1f88eb5f3080a0830b4fbc85fa41107b
jfgomez21 <jfgomez21@gmail.com>
2023-10-28 14:35:39 -0400

    Updating to ADS1115

--- overlay.py
+++ overlay.py
@@ -68,6 +68,24 @@ icons = { "discharging": [ "alert_red", "alert", "20", "30", "30", "50", "60",
                            "charging_60", "charging_60", "charging_80",
                            "charging_90", "charging_full", "charging_full" ]}
 
+RESOLUTION = pow(2, 15) - 1
+MAX_VOLTAGE = 6.144
+R1 = 10000
+R2 = 10000
+
+BATTERY_INPUT_PIN = 0
+CHARGER_INPUT_PIN = 3
+
+CHARGER_LOW_MIN_VOLTAGE = 0.4
+CHARGER_LOW_MAX_VOLTAGE = 1
+
+LOW_VOLTAGE_THRESHOLD = 3.3
+
+class ChargerState(Enum) :
+    STANDBY = 0
+    CHARGING = 1
+    CHARGE_COMPLETE = 2
+
 class InterfaceState(Enum):
   DISABLED = 0
   ENABLED = 1
@@ -80,7 +98,7 @@ class InterfaceState(Enum):
 # 3.2V => will die in 10 mins under load, shut down
 # 3.3V => warning icon?
 
-adc = Adafruit_ADS1x15.ADS1015()
+adc = Adafruit_ADS1x15.ADS1115()
 # Choose a gain of 1 for reading voltages from 0 to 4.09V.
 # Or pick a different gain to change the range of voltages that are read:
 #  - 2/3 = +/-6.144V
@@ -132,12 +150,12 @@ def wifi():
       overlay_processes["wifi"].kill()
       del overlay_processes["wifi"]
 
-    if new_wifi_state == InterfaceState.ENABLED:
-      overlay_processes["wifi"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 2), wifi_icons["enabled"]])
-    elif new_wifi_state == InterfaceState.DISABLED:
-      overlay_processes["wifi"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 2), wifi_icons["disabled"]])
-    elif new_wifi_state == InterfaceState.CONNECTED:
-      overlay_processes["wifi"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 2), wifi_icons["connected"]])
+    #if new_wifi_state == InterfaceState.ENABLED:
+      #overlay_processes["wifi"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 2), wifi_icons["enabled"]])
+    #elif new_wifi_state == InterfaceState.DISABLED:
+      #overlay_processes["wifi"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 2), wifi_icons["disabled"]])
+    #elif new_wifi_state == InterfaceState.CONNECTED:
+      #overlay_processes["wifi"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 2), wifi_icons["connected"]])
   return new_wifi_state
 
 def bluetooth():
@@ -165,12 +183,12 @@ def bluetooth():
       overlay_processes["bt"].kill()
       del overlay_processes["bt"]
 
-    if new_bt_state == InterfaceState.CONNECTED:
-      overlay_processes["bt"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 3), bt_icons["connected"]])
-    elif new_bt_state == InterfaceState.ENABLED:
-      overlay_processes["bt"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 3), bt_icons["enabled"]])
-    elif new_bt_state == InterfaceState.DISABLED:
-      overlay_processes["bt"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 3), bt_icons["disabled"]])
+    #if new_bt_state == InterfaceState.CONNECTED:
+      #overlay_processes["bt"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 3), bt_icons["connected"]])
+    #elif new_bt_state == InterfaceState.ENABLED:
+      #overlay_processes["bt"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 3), bt_icons["enabled"]])
+    #elif new_bt_state == InterfaceState.DISABLED:
+      #overlay_processes["bt"] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * 3), bt_icons["disabled"]])
   return new_bt_state
 
 def environment():
@@ -182,21 +200,28 @@ def environment():
     "freq-capped": bool(val & 0x02),
     "throttled": bool(val & 0x04)
   }
-  for k,v in env.items():
-    if v and not k in overlay_processes:
-      overlay_processes[k] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * (len(overlay_processes)+1)), env_icons[k]])
-    elif not v and k in overlay_processes:
-      overlay_processes[k].kill()
-      del(overlay_processes[k])
+  #for k,v in env.items():
+    #if v and not k in overlay_processes:
+      #overlay_processes[k] = subprocess.Popen(pngview_call + [str(int(resolution[0]) - dpi * (len(overlay_processes)+1)), env_icons[k]])
+    #elif not v and k in overlay_processes:
+      #overlay_processes[k].kill()
+      #del(overlay_processes[k])
   #return env # too much data
   return val
 
+def read_voltage(pin):
+    value = adc.read_adc(pin, gain=2/3)
+    value = (value * MAX_VOLTAGE) / RESOLUTION
+
+    return (value * (R1 + R2)) / R2
+
 def battery():
   global battery_level, overlay_processes, battery_history
-  value = adc.read_adc(0, gain=2/3)
-  value_v = value * 0.003
+
+  value_v = read_voltage(BATTERY_INPUT_PIN)
 
   battery_history.append(value_v)
+
   try:
     level_icon=translate_bat(median(battery_history))
   except IndexError:
@@ -206,8 +231,8 @@ def battery():
   if value_v <= 3.2:
     my_logger.warn("Battery voltage at or below 3.2V. Initiating shutdown within 1 minute")
 
-    subprocess.Popen(pngview_call + [str(int(resolution[0]) / 2 - 64), "-y", str(int(resolution[1]) / 2 - 64), icon_battery_critical_shutdown])
-    os.system("sleep 60 && sudo poweroff &")
+    #subprocess.Popen(pngview_call + [str(int(resolution[0]) / 2 - 64), "-y", str(int(resolution[1]) / 2 - 64), icon_battery_critical_shutdown])
+    #os.system("sleep 60 && sudo poweroff &")
 
   if level_icon != battery_level:
     if "bat" in overlay_processes:
@@ -215,8 +240,19 @@ def battery():
       del overlay_processes["bat"]
 
     icon='ic_battery_' + level_icon + "_white_" + str(dpi) + "dp.png"
-    overlay_processes["bat"] = subprocess.Popen(pngview_call + [ str(int(resolution[0]) - dpi), iconpath + icon])
+    #overlay_processes["bat"] = subprocess.Popen(pngview_call + [ str(int(resolution[0]) - dpi), iconpath + icon])
   return (level_icon, value_v)
+
+def charger():
+    value = read_voltage(CHARGER_INPUT_PIN)
+    state = ChargerState.CHARGE_COMPLETE
+
+    if value < CHARGER_LOW_MIN_VOLTAGE:
+        state = ChargerState.STANDBY
+    elif value >= CHARGER_LOW_MIN_VOLTAGE and value <= CHARGER_LOW_MAX_VOLTAGE:
+        state = ChargerState.CHARGING
+
+    return (state, value)
 
 overlay_processes = {}
 wifi_state = None
@@ -226,7 +262,7 @@ env = None
 battery_history = deque(maxlen=5)
 
 # Set up logging
-my_logger = logging.getLogger('MyLogger')
+my_logger = logging.getLogger('gbzOverlay')
 my_logger.setLevel(logging.INFO)
 handler = logging.handlers.RotatingFileHandler(logfile, maxBytes=102400, backupCount=1)
 my_logger.addHandler(handler)
@@ -238,17 +274,21 @@ resolution=re.search("(\d{3,}x\d{3,})", subprocess.check_output(fbfile.split()).
 my_logger.info(resolution)
 
 while True:
-  (battery_level, value_v) = battery()
-  wifi_state = wifi()
-  bt_state = bluetooth()
-  env = environment()
-  my_logger.info("%s,median: %.2f, %s,icon: %s,wifi: %s,bt: %s, throttle: %#0x" % (
-    datetime.now(),
-    value_v,
-    list(battery_history),
-    battery_level,
-    wifi_state.name,
-    bt_state.name,
-    env
-  ))
-  time.sleep(20)
+    (battery_level, value_v) = battery()
+
+    (charger_state, charger_v) = charger()
+    wifi_state = wifi()
+    bt_state = bluetooth()
+    env = environment()
+
+    my_logger.info("%s,battery: %.2f,charger_state: %s, charger: %.2f,icon: %s,wifi: %s,bt: %s, throttle: %#0x" % (
+        datetime.now(),
+        value_v,
+        charger_state,
+        charger_v,
+        battery_level,
+        wifi_state.name,
+        bt_state.name,
+        env
+    ))
+    time.sleep(5)
